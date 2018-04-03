f3fbff6570033e075d8539af6ffde21b006df8ca
Simon Samnegård <simon@pingpong.net>
2018-04-03 15:40:17 +0200

    Add dialog with contacts on smaller screens

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
     <v-app light>
         <v-content>
-            <section>
+            <section style="position: relative;">
                 <v-parallax src="src/assets/background.jpg" height="700">
                     <v-layout column align-center justify-center class="white--text">
                         <img src="src/assets/logo-full.svg" alt="Optiker Sören Lund" class="mb-2 logo">
                     </v-layout>
                 </v-parallax>
+                <div class="hidden-md-and-up">
+                    <v-btn small absolute dark fab bottom right color="red darken-1" @click.native.stop="dialog = !dialog">
+                        <v-icon>phone</v-icon>
+                    </v-btn>
+                    <v-dialog v-model="dialog" width="600px">
+                        <v-card>
+                            <v-list class="transparent">
+                                <v-list-tile>
+                                    <v-list-tile-action>
+                                        <v-icon>phone</v-icon>
+                                    </v-list-tile-action>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title class="grey--text text--darken-4">0455 - 31 13 34</v-list-tile-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                                <v-list-tile>
+                                    <v-list-tile-action>
+                                        <v-icon>place</v-icon>
+                                    </v-list-tile-action>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title class="grey--text text--darken-4">Arklimästaregatan 39</v-list-tile-title>
+                                        <v-list-tile-sub-title class="grey--text text--darken-1">371 36 Karlskrona</v-list-tile-sub-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                                <v-list-tile>
+                                    <v-list-tile-action>
+                                        <v-icon>email</v-icon>
+                                    </v-list-tile-action>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title><a href="mailto:soren@optikersorenlund.se" title="Skicka mail till oss" class="grey--text text--darken-4">soren@optikersorenlund.se</a></v-list-tile-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                            </v-list>
+                            <v-card-actions>
+                                <v-btn flat color="orange" @click.native="dialog = false">Stäng</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </div>
             </section>
 
             <section class="grey lighten-5 pb-5">
@@ -24,9 +63,9 @@
                         <v-flex xs12 md4>
                             <v-card class="elevation-0 transparent">
                                 <v-card-title primary-title class="layout justify-center">
-                                    <div class="headline text-xs-center">Omtanke</div>
+                                    <div class="headline">Omtanke</div>
                                 </v-card-title>
-                                <v-card-text>
+                                <v-card-text class="px-0">
                                     Hos oss får du personlig service och omtanke!
                                     I en ombonad och unik miljö gör vi en noggrann synundersökning inkl tryckmätning,
                                     därefter får du hjälp att i lugn och ro välja bland våra handplockade bågar från
@@ -39,7 +78,7 @@
                                 <v-card-title primary-title class="layout justify-center">
                                     <div class="headline">Kvalitet</div>
                                 </v-card-title>
-                                <v-card-text>
+                                <v-card-text class="px-0">
                                     Då vi inte tummar på kvalitén använder vi endast högkvalitativa glas från Zeiss.
                                     Givetvis kan vi erbjuda den senaste teknologin inom progressiva glasögon.
                                     Plus kanske något om linser.
@@ -49,9 +88,9 @@
                         <v-flex xs12 md4>
                             <v-card class="elevation-0 transparent">
                                 <v-card-title primary-title class="layout justify-center">
-                                    <div class="headline text-xs-center">Service</div>
+                                    <div class="headline">Service</div>
                                 </v-card-title>
-                                <v-card-text>
+                                <v-card-text class="px-0">
                                    Vi finns i centrum med ett brett utbud av de senaste kollektionerna och välkända varumärken toppat med personlig och professionell service.
                                     Från allra första stund. Hos oss hittar du det senaste modet för alla smaker och personligheter. Välkommen.
                                 </v-card-text>
@@ -160,28 +199,28 @@
                                     <v-layout row wrap justify-space-between align-center>
                                         <v-flex xs12 md3 order-xs1 order-md2>
                                             <v-card flat class="transparent">
-                                                <v-card-media src="src/assets/glasses.png" height="300px"></v-card-media>
+                                                <v-card-media src="src/assets/glasses.png" height="300px" contain></v-card-media>
                                             </v-card>
                                         </v-flex>
                                         <v-flex xs12 md8 order-xs2 order-md1>
                                             <v-card flat color="transparent">
-                                                <v-card-text>
+                                                <v-card-text class="px-0">
                                                     Glasögonen ska sitta skönt, passa din personlighet och vara försedda med glas anpassade för just ditt synfel och din livsstil.
                                                 </v-card-text>
-                                                <v-card-text>
+                                                <v-card-text class="px-0">
                                                     Bågarna och glasen skall tillsammans ge dig det ultimata resultatet och fungera i din vardag och det liv du lever.
                                                 </v-card-text>
-                                                <v-card-text>
+                                                <v-card-text class="px-0">
                                                     Hos oss hittar du ett stort urval av bågar och glas.
                                                     Glas med olika egenskaper och förmågor och bågar i olika former, mönster och färger.
                                                     Allt för att du ska få så stor valfrihet som möjligt.
                                                     Vi handplockar våra bågar själva vilket gör att vi hela tiden kan ha de bästa bågarna ur varje kollektion i vår butik.
                                                 </v-card-text>
-                                                <v-card-text>
+                                                <v-card-text class="px-0">
                                                     Då vi inte tummar på kvalitén använder vi endast högkvalitativa glas från Hoya och Rodenstock.
                                                     Givetvis kan vi erbjuda den senaste teknologin inom progressiva glasögon.
                                                 </v-card-text>
-                                                <v-card-text>
+                                                <v-card-text class="px-0">
                                                     För att du skall hitta din båge hjälper vi dig med utprovning och ärliga smakråd så att du får rätt bågmodell som är klassisk, trendig eller diskret.
                                                     Vi som jobbar i butiken vill att du skall känna dig nöjd med ditt val, känner du dig osäker kan du låna med dig bågar hem.
                                                     Vi slipar och monterar alla glasögon i våra verkstäder, vilket ger dig snabbare leverans av glasögonen.
@@ -195,22 +234,22 @@
                         <v-flex xs12 xl10>
                             <v-container grid-list-md>
                                 <v-layout row wrap align-center justify-center>
-                                    <v-flex xs12 md3>
+                                    <v-flex xs12 sm6 md3>
                                         <v-card class="pa-3" href="https://www.efvaattling.se/sv/artiklar/ovrigt/glasogon/index.html">
                                             <v-card-media src="src/assets/attling.png" height="100px" contain></v-card-media>
                                         </v-card>
                                     </v-flex>
-                                    <v-flex xs12 md3>
+                                    <v-flex xs12 sm6 md3>
                                         <v-card class="pa-3" href="https://www.eoe-glasses.com/">
                                             <v-card-media src="src/assets/eoe.jpg" height="100px" contain></v-card-media>
                                         </v-card>
                                     </v-flex>
-                                    <v-flex xs12 md3>
+                                    <v-flex xs12 sm6 md3>
                                         <v-card class="pa-3" href="https://dcompanynordic.com/sv/varumarke/norr">
                                             <v-card-media src="src/assets/norr.jpg" height="100px" contain></v-card-media>
                                         </v-card>
                                     </v-flex>
-                                    <v-flex xs12 md3>
+                                    <v-flex xs12 sm6 md3>
                                         <v-card class="pa-3" href="https://www.ray-ban.com/sweden">
                                             <v-card-media src="src/assets/rayban.jpg" height="100px" contain></v-card-media>
                                         </v-card>
@@ -235,22 +274,22 @@
                             <v-layout row wrap justify-space-between align-center>
                                 <v-flex xs12 md4>
                                     <v-card flat class="transparent">
-                                        <v-card-media src="src/assets/lenses.png" height="300px"></v-card-media>
+                                        <v-card-media src="src/assets/lenses.png" height="300px" contain></v-card-media>
                                     </v-card>
                                 </v-flex>
                                 <v-flex xs12 md7>
-                                    <v-card flat color="transparent">
-                                        <v-card-text>
+                                    <v-card flat color="transparent" class="pa-0">
+                                        <v-card-text class="px-0">
                                             Upplev friheten med kontaktlinser!
                                             För dig som lever ett aktivt liv är det ett självklart val och fördelarna är många.
                                             Kontaktlinser är aldrig i vägen, de ger ett obegränsat synfält och de återger omgivningen i naturlig storlek utan förvrängningar.
                                         </v-card-text>
-                                        <v-card-text>
+                                        <v-card-text class="px-0">
                                             Utvecklingen av material och passform gör att dagens linser är lätta att vänja sig vid.
                                             De känns inte och de korrigerar ofta synen bättre än glasögon.
                                             I dag kan nästan alla synfelkorrigeras med kontaktlinser: närsynthet, översynthet, astigmatism och åldersynthet.
                                         </v-card-text>
-                                        <v-card-text>
+                                        <v-card-text class="px-0">
                                             Vi har ett komplett utbud av de senaste kontaktlinserna på marknaden: mjuka linser, endagarslinser, dygnetruntlinser, nattlinser (orto-K), topografitillpassade stabila linser och färgade linser.
                                             Är du åldersynt och är i behov av multifokala linser finns det flera olika alternativ och korrigeringar att tillgå.
                                             Är du osäker på vilken typ som passar dig bäst, eller om du använder rätt linser, är du självklart välkommen in för att få en professionell bedömning!
@@ -262,22 +301,22 @@
                         <v-flex xs12 xl10>
                             <v-container grid-list-md>
                                 <v-layout row wrap align-center justify-center>
-                                    <v-flex xs12 md3>
+                                    <v-flex xs12 sm6 md3>
                                         <v-card class="pa-3" href="https://www.efvaattling.se/sv/artiklar/ovrigt/glasogon/index.html">
                                             <v-card-media src="src/assets/alcon.png" height="100px" contain></v-card-media>
                                         </v-card>
                                     </v-flex>
-                                    <v-flex xs12 md3>
+                                    <v-flex xs12 sm6 md3>
                                         <v-card class="pa-3" href="https://dcompanynordic.com/sv/varumarke/norr">
                                             <v-card-media src="src/assets/bauschlomb.png" height="100px" contain></v-card-media>
                                         </v-card>
                                     </v-flex>
-                                    <v-flex xs12 md3>
+                                    <v-flex xs12 sm6 md3>
                                         <v-card class="pa-3" href="https://www.eoe-glasses.com/">
                                             <v-card-media src="src/assets/coopervision.png" height="100px" contain></v-card-media>
                                         </v-card>
                                     </v-flex>
-                                    <v-flex xs12 md3>
+                                    <v-flex xs12 sm6 md3>
                                         <v-card class="pa-3" href="https://www.ray-ban.com/sweden">
                                             <v-card-media src="src/assets/jnj.jpg" height="100px" contain></v-card-media>
                                         </v-card>
@@ -318,7 +357,7 @@
                                 </v-flex>
                                 <v-flex xs12 md7>
                                     <v-card flat color="transparent">
-                                        <v-card-text>
+                                        <v-card-text class="pa-0">
                                             Historik: Optiker Sören Lund har funnits sedan XXXX. Innan dess fanns Optiker Carl Lund i Kiruna, fanns sedan 1982. Togs över av Sören 1996.
                                             Vi arbetar med märken som bla bla bla. Allt för att bla bla bla.
                                         </v-card-text>
@@ -329,8 +368,14 @@
                         <v-flex xs12 lg10>
                             <v-layout row wrap justify-center>
                                 <v-flex xs12 md4 lg6 xl8>
-                                    <p class="pt-4">Vi finns på Arklimästaregatan 39 i Karlskrona, där ”bakom Wachtmeister”. Parkera i P-huset i Wachtmeister och ta med biljetten in så ersätter vi för en timmes parkering.</p>
-                                    <p>Välkommen in till oss, så tar vi tillsammans reda på hur du egentligen ser ut.</p>
+                                    <v-card flat color="transparent">
+                                        <v-card-text class="px-0 pt-0">
+                                            Vi finns på Arklimästaregatan 39 i Karlskrona, där ”bakom Wachtmeister”. Parkera i P-huset i Wachtmeister och ta med biljetten in så ersätter vi för en timmes parkering.</p>
+                                        </v-card-text>
+                                        <v-card-text class="px-0 pb-0">
+                                            Välkommen in till oss, så tar vi tillsammans reda på hur du egentligen ser ut.
+                                        </v-card-text>
+                                    </v-card>
                                 </v-flex>
                                 <v-flex d-flex xs12 sm5 md4 lg3 xl2>
                                     <v-card d-flex>
@@ -436,6 +481,7 @@
     export default {
         data () {
             return {
+                dialog: false
             }
         },
         mounted () {
